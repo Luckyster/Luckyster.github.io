50eea8cd2e3f80937e8b6d11f4cdc62a7aaa2c26
Luckyster <gleb.phrolow@gmail.com>
2019-08-10 13:28:38 +0300

    + update slider
    Add new slider script and styles

--- final/js/script.js
+++ final/js/script.js
@@ -1,37 +1,24 @@
-/*let slides = document.getElementsByClassName("slider__item");
-let dots = document.getElementsByClassName("slider__toggle");
-var slideIndex = 1;
-console.log(slides);
-showSlides(slideIndex);
-addEvents();
-function addEvents() {
-   for(let i = 0; i < dots.length; i++){
-       dots[i].addEventListener('click',currentSlide);
-   }
-}
-
-function currentSlide() {
-    showSlides(parseInt(this.innerHTML));
-    console.log(typeof (parseInt(this.innerHTML)));
-}
-function showSlides(slideIndex) {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" slider__toggle--active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " slider__toggle--active";
-}*/
-//let slides = document.getElementsByClassName("slider__item");
 let slider = document.getElementsByClassName("slider");
-//let dots = document.getElementsByClassName("slider__toggle");
+let dots = document.getElementsByClassName("slider__toggle");
 let arrowLeft = document.getElementsByClassName("slider__arrow--left");
 let arrowRight = document.getElementsByClassName("slider__arrow--right");
+var currentSlide = 0;
+arrowRight[0].onclick = moveSlideRight;
+arrowLeft[0].onclick = moveSlideLeft;
+function moveSlideRight(){
+    moveSlide('right');
 
-arrowRight[0].addEventListener('click',moveSlide);
-//arrowLeft.addEventListener('click',moveSlide);
-function moveSlide(){
-    slider[0].style.left = "-" + 100 + '%';
+}
+function moveSlideLeft(){
+    moveSlide('left');
+}
+function moveSlide(arg){
+    currentSlide = (arg === "right") ? (currentSlide + 1) : (currentSlide - 1);
+    currentSlide === 4 ? currentSlide = 0 : currentSlide === -1 ? currentSlide = 3: false;
+    console.log(currentSlide);
+    slider[0].style.left = "-" + currentSlide * 100 + '%';
+    for(let i = 0; i < dots.length; i++){
+      dots[i].className = dots[i].className.replace(" slider__toggle--active", "");
+    }
+     dots[currentSlide].className += ' slider__toggle--active';
 }
